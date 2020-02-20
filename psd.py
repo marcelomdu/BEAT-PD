9cc66ec0affbdb48e44784cd5e302151d8bc9ecb
Marcelo <marcelomdu@gmail.com>
2020-02-20 12:49:56 -0300

    Test script

--- psd.py
+++ psd.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from numpy.fft import fft
 from scipy import signal
 from scipy.ndimage import gaussian_filter1d as gauss
+import math
 
 
 def calc_fft(x):
@@ -13,10 +14,14 @@ def calc_fft(x):
 
 def load_subj(x,folder):
     subj = pd.read_csv(folder+x+".csv",usecols=(1,2,3))
-    subj['mag'] = subj.pow(2).sum(1).values
-    subj['mag_filt'] = gauss(subj['mag'].values,sigma=0.1)
-    subj['fft'] = calc_fft(subj['mag'].values)
-    return subj
+    mag = np.zeros(subj.values.shape[0]-2)
+    for i in range(1,subj.values.shape[0]-1):
+        diff = subj.values[i,:]-subj.values[i-1,:]
+        mag[i-1] = math.sqrt(pow(diff[0],2)+pow(diff[1],2)+pow(diff[2],2))
+    # subj['mag'] = subj.pow(2).sum(1).values
+    # subj['mag_filt'] = gauss(subj['mag'].values,sigma=0.1)
+    # subj['fft'] = calc_fft(subj['mag'].values)
+    return mag
 
 #-----------------------------------------------------------------------------
     
