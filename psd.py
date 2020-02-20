c610c7a42a2b4eddcdbaf13658dbddc59accebca
Marcelo <marcelomdu@gmail.com>
2020-02-20 19:53:15 -0300

    Script test

--- psd.py
+++ psd.py
@@ -14,14 +14,14 @@ def calc_fft(x):
 
 def load_subj(x,folder):
     subj = pd.read_csv(folder+x+".csv",usecols=(1,2,3))
-    mag = np.zeros(subj.values.shape[0]-2)
-    for i in range(1,subj.values.shape[0]-1):
-        diff = subj.values[i,:]-subj.values[i-1,:]
-        mag[i-1] = math.sqrt(pow(diff[0],2)+pow(diff[1],2)+pow(diff[2],2))
-    # subj['mag'] = subj.pow(2).sum(1).values
+    # mag = np.zeros(subj.values.shape[0]-2)
+    # for i in range(1,subj.values.shape[0]-1):
+        # diff = subj.values[i,:]-subj.values[i-1,:]
+        # mag[i-1] = math.sqrt(pow(diff[0],2)+pow(diff[1],2)+pow(diff[2],2))
+    subj['mag'] = subj.pow(2).sum(1).values
     # subj['mag_filt'] = gauss(subj['mag'].values,sigma=0.1)
     # subj['fft'] = calc_fft(subj['mag'].values)
-    return mag
+    return subj
 
 #-----------------------------------------------------------------------------
     
@@ -32,14 +32,33 @@ folder = "/media/marcelomdu/Data/GIT_Repos/BEAT-PD/Datasets/CIS/Train/training_d
 for x in test_files[:,0]:
     data.append(load_subj(x,folder))
 
-for i in range(0,len(data_jog)):
-    plt.plot(gauss(data_jog[i].values[:,4],sigma=0.5))
-    
+subs0 = {1}
+subs1 = {0,2,4,5}
+subs2 = {6,7}
+subs3 = {3,8,9}
 
+# for i in subs:
+#     plt.plot(data[i].values[:,3])
+    
+    
+freqs = list()
+psd = list()    
+
+plt.figure(figsize=(5, 4))
+for i in subs3:
+    fs, ps = signal.welch(data[i].values[:,3])
+    ps = ps/max(ps)
+    freqs.append(fs)
+    psd.append(ps)
+    plt.semilogx(fs, ps)
+    
+plt.title('PSD: power spectral density')
+plt.xlabel('Frequency')
+plt.ylabel('Power')
+plt.tight_layout()    
+    
 s = 6
 
-sig1 = data_jog[s].values[:,3]
-sig2 = data_walk[s].values[:,3]
 
 freqs, times, spectrogram = signal.spectrogram(sig2)
 
@@ -48,13 +67,4 @@ plt.imshow(spectrogram, aspect='auto', cmap='hot_r', origin='lower')
 plt.title('Spectrogram')
 plt.ylabel('Frequency band')
 plt.xlabel('Time window')
-plt.tight_layout()
-
-freqs, psd = signal.welch(sig1)
-
-plt.figure(figsize=(5, 4))
-plt.semilogx(freqs, psd)
-plt.title('PSD: power spectral density')
-plt.xlabel('Frequency')
-plt.ylabel('Power')
 plt.tight_layout()
