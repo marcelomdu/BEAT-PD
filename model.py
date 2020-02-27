7c34f87abc8b32a10520e7d868b3d1e1fc3bedf3
Marcelo Duarte <marcelomdu@gmail.com>
2020-02-27 19:36:35 -0300

    One shot validation functions definition

--- model.py
+++ model.py
@@ -64,3 +64,46 @@ def get_siamese_model(input_shape):
 
     return siamese_net
 
+
+def make_oneshot_task(val_data,val_labels,N):
+    """Create pairs of test image, support set for testing N way one-shot learning. """
+    _, w, h = val_data.shape
+    
+    test_label = randint(0,5)
+    
+    # Matched and unmatched candidates
+    m_candidates = val_data[val_labels==test_label,:,:]
+    u_candidates = val_data[val_labels!=test_label,:,:]
+    
+    # Random indices for sampling
+    m_idx1, m_idx2 = choice(m_candidates.shape[0]+1,replace=False,size=(2,)) # Non repetitive random indices
+    u_indices = randint(0,u_candidates.shape[0]+1,size=(N,))
+    
+    # Matched image from support_set will be allocated to position '0' then shuffled
+    test_image = np.asarray([m_candidates[m_idx1,:,:]]*N).reshape(N, w, h, 1)
+    support_set = u_candidates[u_indices,:,:]
+    support_set[0,:,:] = m_candidates[m_idx2,:,:]
+    support_set = support_set.reshape(N, w, h, 1)
+    targets = np.zeros((N,))
+    targets[0] = 1
+    targets, test_image, support_set = shuffle(targets, test_image, support_set)
+
+    pairs = [test_image, support_set]
+
+    return pairs, targets
+
+  
+  def test_oneshot(model,val_data,val_labels,N,k, verbose = 0):
+    """Test average N way oneshot learning accuracy of a siamese neural net over k one-shot tasks"""
+    n_correct = 0
+    if verbose:
+        print("Evaluating model on {} random {} way one-shot learning tasks ... \n".format(k,N))
+    for i in range(k):
+        inputs, targets = make_oneshot_task(N)
+        probs = model.predict(inputs)
+        if np.argmax(probs) == np.argmax(targets):
+            n_correct+=1
+    percent_correct = (100.0 * n_correct / k)
+    if verbose:
+        print("Got an average of {}% {} way one-shot learning accuracy \n".format(percent_correct,N))
+    return percent_correct
