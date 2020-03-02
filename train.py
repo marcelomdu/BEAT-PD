78f4204436a050afbbfcf845a283af4729ff9d34
Marcelo Duarte <marcelomdu@gmail.com>
2020-03-02 14:28:50 -0300

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from load_data import load_subject
-from utils import get_batch, test_oneshot
+from utils import get_batch, test_model
 from model import get_siamese_model
 from tensorflow.keras.optimizers import Adam
 from utils import hdf5_handler
@@ -19,11 +19,11 @@ if __name__ == '__main__':
     #pairs, targets, X_train, y_train, X_test, y_test = get_batch(data, labels)
 
     # Hyper parameters
-    evaluate_every = 2 # interval for evaluating on one-shot tasks
+    evaluate_every = 20 # interval for evaluating on one-shot tasks
     #batch_size = 32
-    n_iter = 20 # No. of training iterations
-    N_way = 3 # how many classes for testing one-shot tasks
-    n_val = 250 # how many one-shot tasks to validate on
+    n_iter = 2000 # No. of training iterations
+    #N_way = 3 # how many classes for testing one-shot tasks
+    #n_val = 250 # how many one-shot tasks to validate on
     best = -1
 
 
@@ -36,19 +36,23 @@ if __name__ == '__main__':
 
     print("Start Training")
     print("-------------------------------------")
-    t_start = time.time()
-    for i in range(1, n_iter+1):
-        inputs, targets, X_train, y_train, X_test, y_test = get_batch(data, labels)
-        loss = model.train_on_batch(inputs, targets)
-        print("Train Loss: {0}".format(loss)+" Iter: {}".format(i))
-        # if i % evaluate_every == 0:
-        #     print("\n ------------- \n")
-        #     print("Time for {0} iterations: {1} mins".format(i, (time.time()-t_start)/60.0))
-        #     print("Train Loss: {0}".format(loss)) 
-        #     val_acc = test_oneshot(model, X_test, y_test, N_way, n_val, verbose=True)
-        #     model.save_weights(os.path.join(model_path, 'weights.{}.h5'.format(i)))
-        #     if val_acc >= best:
-        #         print("Current best: {0}, previous best: {1}".format(val_acc, best))
-        #         best = val_acc
-            
+    
+    for subject_id in subjects_ids:
+        data = f[str(subjects_ids)]['measurements'][:][:,:]
+        labels = f[str(subjects_ids)]['labels'][:]
+        t_start = time.time()
+        for i in range(1, n_iter+1):
+            inputs, targets, X_train, y_train, X_test, y_test = get_batch(data, labels)
+            loss = model.train_on_batch(inputs, targets)
+            print("Train Loss: {0}".format(loss)+" Iter: {}".format(i))
+            if i % evaluate_every == 0:
+                print("\n ------------- \n")
+                print("Time for {0} iterations: {1} mins".format(i, (time.time()-t_start)/60.0))
+                print("Train Loss: {0}".format(loss)) 
+                val_acc = test_model(model, X_test, y_test)
+                model.save_weights(os.path.join(model_path, 'weights.{}.h5'.format(i)))
+                if val_acc >= best:
+                    print("Current best: {0}, previous best: {1}".format(val_acc, best))
+                    best = val_acc
+
 
